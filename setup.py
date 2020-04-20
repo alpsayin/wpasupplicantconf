9f8b0440bec6b0b074c9eee4d4363f71c98ff0c2
termidor0 Pi <termidor0@novit.ai>
2020-04-20 12:06:04 +0100

    add myself to authors list, increment version number

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='wpasupplicantconf',
-    version='0.1',
+    version='0.2',
     description='Parsing, manipulation and generation of wpa_supplicant.conf files',
-    author='Menno Finlay-Smits',
+    author='Menno Finlay-Smits, Alp Sayin',
     author_email='menno@cacophony.org.nz',
-    url='https://github.com/TheCacophonyProject/wpasupplicantconf',
+    url='https://github.com/alpsayin/wpasupplicantconf',
     py_modules=['wpasupplicantconf'],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
     ],
